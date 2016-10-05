5ac800bb803471acf8403cc68d7b20dfd60e921f
Lucas Ricoy <ricoy.lucas@gmail.com>
2016-10-04 21:37:52 -0300

    MaxCounter solution (100%)

--- solutions/MaxCounters/MaxCounters.js
+++ solutions/MaxCounters/MaxCounters.js
@@ -1,21 +1,28 @@
 function solution(N, A) {
     let counters = Array(N).fill(0);
+    let lastMax = 0;
     let currentMax = 0;
     const increase = (x) => {
+        if(counters[x] < lastMax) {
+            counters[x] = lastMax;
+        }        
         counters[x] += 1;
-        if(counters[x] > currentMax)
+        if(counters[x] > currentMax) {
             currentMax = counters[x];
+        }        
     };
-    const maxCounter = () => {
-        counters = Array(N).fill(currentMax);
+    const maxCounters = () => {
+        counters = counters
+            .map(x => x < lastMax? lastMax : x);
     };
-    
+
     for(let i=0; i < A.length; i++) {
-        if(A[i] <= N) 
+        if(A[i] <= N)
             increase(A[i] -1);
-        else
-            maxCounter();
+        else {
+            lastMax = currentMax;
+        }
     }
-    
+    maxCounters();
     return counters;
 }
